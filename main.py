105518cb420a8d6b3e3a9f2e866fd335259d801b
saintcoder0 <tusharsharmalandtoearth@gmail.com>
2026-09-05 22:27:05 +0530

    another day for learning https methods

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"hello":"world"}
+
+
+items = {"foo": "The Foo Wrestlers" ,
+    "how": "the love of world" ,
+    "metallica": "hard death"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id:str):
+    return {item_id: items[item_id]}
+
+
+@app.put("/items/{item_id}")
+def create_item(item_id: str, item_name: str):
+    items[item_id] = item_name
+    return {item_id: items[item_id]}
